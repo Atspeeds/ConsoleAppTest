8081d28f381a5d927d4d62b1d28d510296579e88
Amir Kharazi <irantehranid@yahoo.com>
2024-08-16 21:57:14 +0330

    CreateDetails

---
+++ ConsoleApp1/Details.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Details
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public Details(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+    }
+}
